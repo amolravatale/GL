87e876e974b12005431664084cf18cfc284d2a2e
amolravatale <amolravatale@rediffmail.com>
2020-09-05 21:34:13 +0530

    Changes

--- Practise/pyCharmProjects/pgp-aiml-feb20-UnSupervisedLearning_R4_InternalLab.py
+++ Practise/pyCharmProjects/pgp-aiml-feb20-UnSupervisedLearning_R4_InternalLab.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import numpy as np
 
-def Oper(opt, x,y) :
-    if(opt==1) :
-        return
+val1 = "012345"
+
+
+
+
+
+# for i in range(0, len(val)) :
+#    print(val[i])
+def reverse(val):
+    if len(val) == 1:
+        return val[0]
+    else:
+        return reverse(val[1:len(val)]) + val[0]
+
+
+print(reverse(val1))
